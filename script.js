1d7991bab96ebc95cb02fad1679fc7fa29a91221
GavinEscanilla <gavinaltonescanilla@gmail.com>
2024-01-25 02:14:30 -0500

    started the functionality

---
+++ script.js
@@ -0,0 +1,6 @@
+const playerR = document.getElementById('playerRock');
+const playerP = document.getElementById('playerPaper');
+const playerS = document.getElementById('playerScissor');
+const AiR = document.getElementById('AIRock');
+const AiP = document.getElementById('AIPaper');
+const AiS = document.getElementById('AIScissor');
